c50f27048de8acfd04c3817d2a93110c49f67d5b
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-10-23 23:46:43 +0300

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Preloader from "./components/Preloader";
+import Navbar from "./components/Navbar";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import Feed from "./routes/Feed";
+import Home from "./routes/Home";
+import Jobs from "./routes/Jobs";
+import Messaging from "./routes/Messaging";
+import Network from "./routes/Network";
+import Notifications from "./routes/Notifications";
+import Profile from "./routes/Profile";
+import NotFound from "./routes/Notfound";
+import Register from "./routes/Register";
+import Login from "./routes/Login";
 
 function App() {
+  const [curPath, setCurPath] = useState(true);
+  //page pre loader
+  document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+      document.querySelector(".Pagecontents").style.visibility = "hidden";
+      document.querySelector(".ppreloader").style.display = "unset";
+    } else {
+      document.querySelector(".ppreloader").style.display = "none";
+      document.querySelector(".Pagecontents").style.visibility = "visible";
+      document.querySelector(".Pagecontents").style.display = "unset";
+    }
+  };
+  //home remove navbar
+  const loc = useLocation();
+  useEffect(() => {
+    const rendernav = () => {
+      if (
+        loc.pathname === "/home" ||
+        loc.pathname === "/login" ||
+        loc.pathname === "/register"
+      ) {
+        setCurPath(false);
+      }
+    };
+    rendernav();
+  }, [loc]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ppreloader">
+        <Preloader />
+      </div>
+      <div className="Pagecontents">
+        {curPath && <Navbar />}
+
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/feed" element={<Feed />}></Route>
+            <Route path="/" element={<Navigate to="/feed" />}></Route>
+            <Route path="/jobs" element={<Jobs />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/messaging" element={<Messaging />}></Route>
+            <Route path="/network" element={<Network />}></Route>
+            <Route path="/notifications" element={<Notifications />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
